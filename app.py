cc8fc97bfb78a1d49c05f307fbe46635e299a092
jwu611 <jane.wu@hotmail.ca>
2021-08-21 17:59:20 -0700

    Performance improvements: Changed detection to process every 3rd frame+stopped detection when alarm is disabled

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Import necessary libraries
 import imutils
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from dlib_sleep_detector import SLEEP_THRESHOLD_SECS, detect_draw_eyes, set_uploaded_alarm
 from imutils.video import VideoStream
 from imutils.video import FileVideoStream
@@ -8,13 +8,19 @@ from imutils import face_utils
 import cv2
 import time
 from datetime import datetime
+
+DETECTION_ON = True
+
 #Initialize the Flask app
 app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)
 time.sleep(1)
-def generate():  
+def generate():
+    global DETECTION_ON
+    frame_count = 0  
     while True:
+        #time.sleep(0.5)
         success, frame = camera.read()  # read the camera frame
         if not success:
             camera.release()
@@ -22,14 +28,19 @@ def generate():
         else:
             frame = imutils.resize(frame, width=450)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            timediff, frame = detect_draw_eyes (frame, gray)
-            if timediff.total_seconds() >= SLEEP_THRESHOLD_SECS:
-                camera.release()
-                break
+
+            if DETECTION_ON:
+                if frame_count % 3 == 0:
+                    timediff, frame = detect_draw_eyes (frame, gray)
+                    if timediff.total_seconds() >= SLEEP_THRESHOLD_SECS:
+                        DETECTION_ON = False
+                        #camera.release()
+                        #break
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+        frame_count += 1
 
 @app.route('/')
 def index():
